8052774cd39e8e33747c1be678e7f250b2fcbf60
Angelicogfa <angelicogfa@gmail.com>
2017-12-13 16:34:51 -0200

    Alteração em codigo teste

--- PaymentContext.Test/Entities/StudentTest.cs
+++ PaymentContext.Test/Entities/StudentTest.cs
@@ -9,7 +9,7 @@ namespace PaymentContext.Test.Entities
     public class StudentTest
     {
         [Fact]
-        public void AdicionarAssinatura()
+        public void AdicionarAssinaturaValida()
         {
             Subscription subscription = new Subscription(null);
             Student student = new Student(new Name("Jose", "Silva"), new Document("123.456.789-96", EDocumentType.CPF), new Email("jose_silva@gmail.com"));
@@ -19,6 +19,20 @@ namespace PaymentContext.Test.Entities
             Assert.True(student.Valid);
             Assert.True(subscription.Valid);
             Assert.Contains(subscription, student.Subscriptions);
+        }
+
+        [Fact]
+        public void AdicionarAssinaturaInvalida_ExistenciaSubscription()
+        {
+            Student student = new Student(new Name("Jose", "Silva"), new Document("123.456.789-96", EDocumentType.CPF), new Email("jose_silva@gmail.com"));
+
+            student.AddSubscription(new Subscription(null));
+            Assert.True(student.Valid);
+            Assert.True(student.Subscriptions.Count == 1);
+
+            student.AddSubscription(new Subscription(null));
+            Assert.True(student.Invalid);
+            Assert.True(student.Subscriptions.Count == 1);
         }
     }
 }
